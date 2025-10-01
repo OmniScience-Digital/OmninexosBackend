81080a35305ba67fe1047a729efc03d9e1119933
ThabisoBlessed <thabiso@omniscience.digital>
2025-10-01 11:11:11 +0200

    Initial commit

--- src/routes/clickup.route.ts
+++ src/routes/clickup.route.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { clickUpRouter } from '../controllers/clickup.controller';
 
 const router = express.Router();
-
 router.post('/clickuppost', clickUpRouter);
 
 export default router;
