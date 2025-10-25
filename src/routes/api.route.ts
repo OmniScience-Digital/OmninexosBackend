164e073b7d225499ef0017138a5d40ccda4e01f0
ThabisoBlessed <thabiso@omniscience.digital>
2025-10-25 21:15:04 +0200

    removing vif1

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -4,9 +4,7 @@ const routes = express.Router();
 import stockControllerApi from './stock.route';
 import clickupApi from './clickup.route';
 
-
 routes.use('/api/v1', stockControllerApi);
 routes.use('/api/v1', clickupApi);
-
 
 export default routes;
