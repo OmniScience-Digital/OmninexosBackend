6bb291eb1a7e619b37f80ca74e9c6b55a999806b
OmniScience-Digital <techstack@omniscience.digital>
2026-02-20 13:16:02 +0200

    Handling webhook key

--- src/index.ts
+++ src/index.ts
@@ -7,42 +7,44 @@ import executiontime from './middlewares/execution.middleware';
 import errorhandling from './middlewares/errorhandling.middleware';
 import routes from './routes/api.route';
 
-const port = process.env.PORT;
+const config = {
+  port: process.env.PORT,
+  host: process.env.HOST,
+};
+
+//Server Port
+const port = config.port;
 const app = express();
 
+// Trust the proxy
 app.set('trust proxy', true);
 
-/* ===============================
-   IMPORTANT:XERO WEBHOOK RAW BODY
-================================= */
-app.use('/api/v1/xero/xeroBillwebhook', express.raw({ type: 'application/json' }));
-
-/* ===============================
-   Normal Body Parsers (after)
-================================= */
-app.use(express.json({ limit: '10mb' }));
+// Middleware to parse JSON bodies
+app.use(express.json({ limit: '10mb' })); // Increase limit as needed
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-/* =============================== */
+//Enable Cors
 app.use(
   cors({
     origin: '*',
-    methods: 'GET,POST',
+    methods: 'Get,POST',
     credentials: true,
   })
 );
 
+//json compression
 app.use(compression());
 
-/* ===============================
-   Routes
-================================= */
+//register routes
 app.use('/', routes);
 
-app.listen(port, () => {
-  logger.info(`App is running at http://localhost:${port}`);
+app.listen(port, async () => {
+  logger.info(`App is running  at http://localhost:${port}`);
   logger.info(`Running on env : ${process.env.NODE_ENV}`);
 });
 
+//logging middleware
 executiontime(app);
+
+//Error handling middleware
 errorhandling(app);
